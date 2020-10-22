29675ee2e0d4294300a91c6c329da38d9a6a4dd9
Gustavo Silva <gustavo-silva14@hotmail.com>
2020-10-22 16:48:26 -0300

    commit estudo dia 22/10

---
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+
+            try
+            {
+
+                Contato contato = new Contato();
+
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var ListaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, ListaMensagens,true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach(var texto in ListaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br/>");
+                    }
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+
+                
+            }
+            catch(Exception e)
+            {
+                ViewData["MSG_E"] = "Houve um erro, tente novamente mais tarde!";
+
+                //TODO - Implementar log
+            }
+
+
+
+
+
+            return View("Contato");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Cliente
+    {
+        /* PK */
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string CPF { get; set; }
+        public string Sexo { get; set; }
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+        public int Senha { get; set; }
+
+
+    }
+}
